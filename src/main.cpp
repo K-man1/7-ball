c12fed9969df7425d856da4de1d6ba946b1a70cd
K-man1 <karman.chhatwal@gmail.com>
2026-02-28 12:20:35 -0500

    working solo awp

--- src/main.cpp
+++ src/main.cpp
@@ -223,6 +223,14 @@ void competition_initialize() {}
 // ASSET(path2loader2_txt); // '.' replaced with "_" to make c++ happy
 
 
+
+
+
+
+
+
+
+
 /**
  * Runs during auto
  *
@@ -314,7 +322,7 @@ void autonomous() {
 
 
     //go to align to 2nd part of auto
-    chassis.moveToPoint(-5, -63, 830);
+    chassis.moveToPoint(-5, -64, 830);
 
 
     //turn to face goal
@@ -322,25 +330,30 @@ void autonomous() {
 
 
     //move back into goal and score
-    chassis.moveToPoint(-23, -63, 1100, {.forwards = false});
+    chassis.moveToPoint(-23, -64, 1100, {.forwards = false});
     pros::delay(300);
     intake.move(-127);
-    pros::delay(100);
+    pros::delay(300);
     score();
     will.toggle();
     pros::delay(1000);
-    chassis.setPose(-23, -63, chassis.getPose().theta);
+    chassis.setPose(-23, -64, chassis.getPose().theta);
 
     //go into loader
-    chassis.moveToPose(20, -61, 90, 1000, {.maxSpeed = 75});
+    chassis.moveToPose(20, -64, 90, 1000, {.maxSpeed = 65});
     load();
-    pros::delay(1200);
+    chassis.turnToHeading(93, 100);
+    chassis.turnToHeading(87, 100);
+    chassis.turnToHeading(93, 100);
+    chassis.turnToHeading(87, 100);
+    chassis.turnToHeading(90, 1200);
+
 
     //back away
-    chassis.moveToPoint(0, -61, 1000, {.forwards = false});
+    chassis.moveToPoint(0, -64, 1000, {.forwards = false});
 
     //go to middle goal
-    chassis.moveToPose(-45, -21, 135, 5000, {.forwards = false});
+    chassis.moveToPose(-43, -21, 135, 5000, {.forwards = false});
     pros::delay(1500);
     intake.move(-127);
     pros::delay(100);
